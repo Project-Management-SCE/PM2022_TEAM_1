ef3b73340a5431d893f0d7dab9da8e8223976364
salemgu1 <74470511+salemgu1@users.noreply.github.com>
2022-04-17 13:51:52 +0300

    test

--- track/tests.py
+++ track/tests.py
@@ -1,19 +1,43 @@
+import unittest
+from multiprocessing.dummy.connection import Client
+
 from django.test import TestCase, tag
 from django.urls import reverse
-from django.test import Client
 from track.models import *
 
 
-class AdminTest(TestCase):
-    @tag('unit-test')
-    def admin_Add_Nurse_GET(self):
-        c = Client()
-        response = c.get(reverse('admin-add-nurse'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'admin_add_nurse.html')
 
-    @tag('unit-test')
-    def test_login(self):
-        login = self.client.login(username='test', password='test')
-        self.assertFalse(login)
 
+@tag('unit-test')
+def test_login(self):
+    login = self.client.login(username='israa1', password='123')
+    self.assertFalse(login)
+
+class LogoutTest(TestCase):
+   def testLogout(self):
+       User.objects.create(username='israa1', password='123')
+       self.client.login(username='username',password='password')
+
+       response = self.client.get(reverse('logout'), follow=True)
+
+       self.assertEqual(response.status_code, 200)
+       self.assertFalse(response.context["user"].is_authenticated)
+
+
+# class LogoutTest(TestCase):
+#    def testLogout(self):
+#        User.objects.create(username='username', password='username')
+#        self.client.login(username='username',password='password')
+#
+#        response = self.client.get(reverse('logout'), follow=True)
+#
+#        self.assertEqual(response.status_code, 200)
+#        self.assertFalse(response.context["user"].is_authenticated)
+
+class ManageUsersTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='username', email='email',
+                                        last_name='last_name',
+                                        first_name='first_name')
+        self.user.set_password('password')
+        self.user.save()

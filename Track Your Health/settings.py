37204e0fd35b90ffec7532ae607d6bba1509f9c6
salemgu1 <74470511+salemgu1@users.noreply.github.com>
2022-05-24 12:31:32 +0300

    Update settings.py

--- Track Your Health/settings.py
+++ Track Your Health/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'track',
+    'track.apps.TrackConfig',
     'widget_tweaks',
 ]
 
